bb502d61e5c1e7a7cba13a26797ed288f6a43e75
saturdayxiii <82848893+saturdayxiii@users.noreply.github.com>
2026-02-17 09:30:03 +0100

    Create robocopy_paste_launcher.ps1

---
+++ robocopy_paste_launcher.ps1
@@ -0,0 +1,22 @@
+$Destination = $args[0]
+$Mode        = $args[1]
+
+# Detect Scoop location
+if ($env:SCOOP -and (Test-Path -LiteralPath $env:SCOOP)) {
+    $ScoopRoot = $env:SCOOP
+}
+else {
+    $ScoopRoot = Join-Path $env:USERPROFILE "scoop"
+}
+
+$ScriptPath = Join-Path $ScoopRoot "apps\robocopycontext\current\robocopy_paste_profile.ps1"
+
+if (!(Test-Path -LiteralPath $ScriptPath)) {
+    Write-Host "Robocopy Tools not found in Scoop directory."
+    Write-Host "Checked:"
+    Write-Host $ScriptPath
+    Pause
+    exit
+}
+
+& $ScriptPath $Destination $Mode
